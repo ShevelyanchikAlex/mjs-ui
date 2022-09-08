5bb65a004d428f814eefb78aadec04fd89d78fdf
Aliaksandr Shavialianchyk <aliaksandr_shavialianchyk@epam.com>
2022-09-08 11:26:58 +0300

    added search by name, description and tags

--- assets/js/header-dropdown.js
+++ assets/js/header-dropdown.js
@@ -1,15 +1,5 @@
 'use strict';
 
-const selectedItem = document.querySelector('.selected-item');
-const dropdown = document.querySelector('.dropdown');
-const items = dropdown.querySelectorAll('.item');
-
-items.forEach(item => {
-    item.addEventListener('change', () => {
-
-        if (item.checked) {
-            selectedItem.innerHTML = item.value;
-            dropdown.open = false;
-        }
-    });
-});
+function changeSelectedItem(selectedItemText) {
+    document.querySelector('.textBox').value = selectedItemText;
+}
--- assets/js/search-filter.js
+++ assets/js/search-filter.js
@@ -5,9 +5,12 @@ function filterBySearchInput() {
     let filter = input.value.toUpperCase();
     let gridView = document.getElementById("items-grid-view");
     let elementsList = gridView.getElementsByTagName('div');
+    let selectedSearchItem = document.querySelector('.textBox').value;
+    let elementsClassName = selectedSearchItem === 'Name' ? "card-name" :
+        selectedSearchItem === 'Description' ? "card-description" : "tags";
 
     for (let i = 0; i < elementsList.length; i++) {
-        let items = elementsList[i].getElementsByClassName("card-name")[0];
+        let items = elementsList[i].getElementsByClassName(elementsClassName)[0];
         let txtValue = items.textContent || items.innerText;
         if (txtValue.toUpperCase().indexOf(filter) > -1) {
             elementsList[i].style.display = "";
